4f579777302180ea465ce9ea6c5a30da0bc6d88e
claramarievela-ai <claramarievela@gmail.com>
2026-03-04 18:16:11 -0500

    Added JavaScript DOM events

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function changeColor() {
+  document.getElementById("main-header").style.backgroundColor = "hotpink";
+}
+function changeText() {
+  document.getElementById("main-content").innerHTML = "I love coding!";
+}
+function toggleAside() {
+  var aside = document.querySelector("aside");
+  if (aside.style.display === "none") {
+    aside.style.display = "block";
+  } else {
+    aside.style.display = "none";
+  }
+}
+function makeYellow() {
+  document.getElementById("main-header").style.backgroundColor = "yellow";
+}
+
+function makeBlue() {
+  document.getElementById("main-header").style.backgroundColor =
+    "rgb(142, 182, 247)";
+}
